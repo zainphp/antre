4f9653b08f1db53904ac27cddf670a732119558c
🍕 <zainadam.id@gmail.com>
2026-09-21 15:31:27 +0800

    feat: update favicon and manifest sizes for improved display

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,9 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
+        <link rel="icon" href="/favicon.ico" sizes="32x32">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180">
         <link rel="manifest" href="/manifest.webmanifest">
 
         @fonts
